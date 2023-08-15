ef6720695dfb0b35daceb969b82b7836add92643
jooker302 <i.omer6637@gmail.com>
2023-08-15 13:08:23 -0700

    First

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      
+      <link rel="icon" href="main.jpeg" />
+    </head>
       <body >{children}</body>
     </html>
   )
