9f6afe35080d9d7e34fa2241176082ac1745c2d2
jooker302 <i.omer6637@gmail.com>
2023-08-15 13:02:49 -0700

    First

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Pink Bird',
+  title: 'Pink Bird ',
   description: 'Pink Bird (DxSale)',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   )
 }
